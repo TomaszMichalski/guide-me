f7493bc8e94e2a1d68e5a7e710c0b816a2375623
TomaszMichalski <tomasz.michalski19@gmail.com>
2019-04-20 12:51:49 +0200

    Addresses now contain country

--- guide-me-server/src/main/java/guide/me/server/database/loader/DatabaseLoader.java
+++ guide-me-server/src/main/java/guide/me/server/database/loader/DatabaseLoader.java
@@ -20,21 +20,21 @@ public class DatabaseLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Category universities = new Category("Uczelnie");
-        Place agh = new Place("Akademia Górniczo-Hutnicza", "al. Mickiewicza 30, 30-059 Kraków",
+        Place agh = new Place("Akademia Górniczo-Hutnicza", "al. Mickiewicza 30, 30-059 Kraków, PL",
                 50.052652, 19.987345, universities);
-        Place uek = new Place("Uniwersytet Ekonomiczny", "Rakowicka 27, 31-510 Kraków",
+        Place uek = new Place("Uniwersytet Ekonomiczny", "Rakowicka 27, 31-510 Kraków, PL",
                 50.068464, 19.953883, universities);
-        Place pk = new Place("Politechnika Krakowska", "ul. Warszawska 24, 31-355 Kraków",
+        Place pk = new Place("Politechnika Krakowska", "ul. Warszawska 24, 31-355 Kraków, PL",
                 50.052652, 19.987345, universities);
 
         Category busStations = new Category("Dworce autobusowe");
-        Place mainStation = new Place("Małopolski Dworzec Autobusowy", "ul. Bosacka 18, 31-505 Kraków",
+        Place mainStation = new Place("Małopolski Dworzec Autobusowy", "ul. Bosacka 18, 31-505 Kraków, PL",
                 50.052652, 19.987345, busStations);
-        Place czyzynyStation = new Place("Dworzec Autobusowy Czyżyny", "Medweckiego 15, 31-863 Kraków",
+        Place czyzynyStation = new Place("Dworzec Autobusowy Czyżyny", "Medweckiego 15, 31-863 Kraków, PL",
                 50.052652, 19.987345, busStations);
 
         Category museums = new Category("Muzea");
-        Place national = new Place("Muzeum Narodowe", "al. 3 Maja 1, 30-062 Kraków",
+        Place national = new Place("Muzeum Narodowe", "al. 3 Maja 1, 30-062 Kraków, PL",
                 50.060431, 19.923620, museums);
 
         categoryRepository.save(universities);

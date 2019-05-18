0d2331370e7d8b56177ac19a016b27c22639e0c5
TomaszMichalski <tomasz.michalski19@gmail.com>
2019-05-18 18:04:09 +0200

    POST reworking, again

--- src/main/java/guide/me/server/controller/PlacesController.java
+++ src/main/java/guide/me/server/controller/PlacesController.java
@@ -5,13 +5,18 @@ import guide.me.server.model.Place;
 import guide.me.server.repository.CategoryRepository;
 import guide.me.server.repository.PlaceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
+@RequestMapping("/api")
 public class PlacesController {
 
     @Autowired
@@ -21,12 +26,24 @@ public class PlacesController {
     private PlaceRepository placeRepository;
 
     @PostMapping("/places")
-    public Place createPlace(@RequestBody Place place) {
-        return placeRepository.save(place);
+    public ResponseEntity<Object> createPlace(@RequestBody Place place) {
+        Place savedPlace = placeRepository.save(place);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedPlace.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
     @PostMapping("/categories")
-    public Category createCategory(@RequestBody Category category) {
-        return categoryRepository.save(category);
+    public ResponseEntity<Object> createCategory(@RequestBody Category category) {
+        Category savedCategory = categoryRepository.save(category);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedCategory.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }

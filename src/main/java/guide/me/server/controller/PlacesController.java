b6237c2a76e138c7d261cbba444aa0838c4a1271
TomaszMichalski <tomasz.michalski19@gmail.com>
2019-05-18 17:13:05 +0200

    POST mappings for places and categories

---
+++ src/main/java/guide/me/server/controller/PlacesController.java
@@ -0,0 +1,46 @@
+package guide.me.server.controller;
+
+import guide.me.server.model.Category;
+import guide.me.server.model.Place;
+import guide.me.server.repository.CategoryRepository;
+import guide.me.server.repository.PlaceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api")
+public class PlacesController {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private PlaceRepository placeRepository;
+
+    @PostMapping("/places")
+    public ResponseEntity<Object> createPlace(@RequestBody Place place) {
+        Place savedPlace = placeRepository.save(place);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedPlace.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @PostMapping("/categories")
+    public ResponseEntity<Object> createCategory(@RequestBody Category category) {
+        Category savedCategory = categoryRepository.save(category);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedCategory.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+}

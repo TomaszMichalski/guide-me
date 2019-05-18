a318a114c3c47b0b8b3161f84357077264f6ce5f
TomaszMichalski <tomasz.michalski19@gmail.com>
2019-05-18 17:22:57 +0200

    GET request, reworked POST requests

--- src/main/java/guide/me/server/controller/PlacesController.java
+++ src/main/java/guide/me/server/controller/PlacesController.java
@@ -5,14 +5,11 @@ import guide.me.server.model.Place;
 import guide.me.server.repository.CategoryRepository;
 import guide.me.server.repository.PlaceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
 
 @RestController
 @RequestMapping("/api")
@@ -24,23 +21,23 @@ public class PlacesController {
     @Autowired
     private PlaceRepository placeRepository;
 
-    @PostMapping("/places")
-    public ResponseEntity<Object> createPlace(@RequestBody Place place) {
-        Place savedPlace = placeRepository.save(place);
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(savedPlace.getId()).toUri();
+    @GetMapping("/places")
+    public Iterable<Place> allPlaces() {
+        return placeRepository.findAll();
+    }
 
-        return ResponseEntity.created(location).build();
+    @PostMapping("/places")
+    public Place createPlace(@RequestBody Place place) {
+        return placeRepository.save(place);
+    }
+
+    @GetMapping("/categories")
+    public Iterable<Category> allCategories() {
+        return categoryRepository.findAll();
     }
 
     @PostMapping("/categories")
-    public ResponseEntity<Object> createCategory(@RequestBody Category category) {
-        Category savedCategory = categoryRepository.save(category);
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(savedCategory.getId()).toUri();
-
-        return ResponseEntity.created(location).build();
+    public Category createCategory(@RequestBody Category category) {
+        return categoryRepository.save(category);
     }
 }

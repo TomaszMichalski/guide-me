801249f8c144de7a92f8ee125cb6a0807393bfc9
Tomasz Michalski <34740417+TomaszMichalski@users.noreply.github.com>
2019-05-26 12:59:36 +0200

    Travis fix (#3)
    * travis conf change
    
    * Fix for local db instance

--- src/main/java/guide/me/server/database/config/DatabaseConfig.java
+++ src/main/java/guide/me/server/database/config/DatabaseConfig.java
@@ -5,19 +5,30 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
 @Configuration
+@Profile("heroku")
 public class DatabaseConfig {
 
-    @Value("${spring.datasource.url}")
+    @Value("${JDBC_DATABASE_URL}")
     private String dbUrl;
+
+    @Value("${JDBC_DATABASE_USERNAME}")
+    private String dbUser;
+
+    @Value("${JDBC_DATABASE_PASSWORD}")
+    private String dbPassword;
 
     @Bean
     public DataSource dataSource() {
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(dbUrl);
+        config.setUsername(dbUser);
+        config.setPassword(dbPassword);
+        config.setDriverClassName("org.postgresql.Driver");
 
         return new HikariDataSource(config);
     }

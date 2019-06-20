b4099bd6043255e35926bb02a8204bfc06a4b08b
dyduch <dominik.dyduch@gmail.com>
2019-06-20 21:56:48 +0200

    added cards with pictures

--- guide-me-client/src/categories/Categories.js
+++ guide-me-client/src/categories/Categories.js
@@ -1,4 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React, { Component } from 'react';
+import Muzea from '../img/Muzea.jpg';
+import Uczelnie from '../img/Uczelnie.jpg';
+import Dworce from '../img/Dworce.jpg';
+import Galerie from '../img/Galerie.jpg';
+import './Categories.css';
+
+
 
 class Category extends Component {
     constructor(props) {
@@ -6,16 +14,27 @@ class Category extends Component {
         console.log(props);
     }
     render() {
+        const picUrl = Category.resolvePic(this.props.category.name);
         return (
-            <tr>
-                <td>{this.props.category.id}</td>
-                <td>{this.props.category.name}</td>
-                <td>{this.props.category.address}</td>
-                <td>{this.props.category.latitude}</td>
-                <td>{this.props.category.longitude}</td>
+                <div className="card flex-item" style={{width: "18rem"}}>
+                    <img className="card-img-top" src={picUrl} alt="Card image cap"/>
+                        <div className="card-body">
+                            <h5 className="card-title">{this.props.category.name}</h5>
+                            <a href="#" className="btn btn-primary btn-block">Dodaj</a>
+                            <a href="#" className="btn btn-block" style={{background: "red", color: "white"}}>Usun</a>
 
-            </tr>
+                        </div>
+                </div>
         )
+    }
+
+    static resolvePic(name) {
+        switch (name) {
+            case "Uczelnie": return Uczelnie;
+            case "Muzea": return Muzea;
+            case "Dworce autobusowe": return Dworce;
+            case "Galerie handlowe": return Galerie;
+        }
     }
 }
 
@@ -31,18 +50,10 @@ class CategoryList extends Component {
             <Category key={category._links.self.href} category={category}/>
         );
         return (
-            <table>
-                <tbody>
-                <tr>
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>Address</th>
-                    <th>Latitude</th>
-                    <th>Longitude</th>
-                </tr>
+                <div className="flex-container">
                 {categories}
-                </tbody>
-            </table>
+                </div>
+
         )
     }
 }
